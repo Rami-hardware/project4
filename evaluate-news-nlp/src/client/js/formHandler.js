dff7f11f749d2ff883b5f20492716f799f239676
Rami <killx51@hotmail.com>
2022-10-21 20:01:16 +0300

    fixing

--- evaluate-news-nlp/src/client/js/formHandler.js
+++ evaluate-news-nlp/src/client/js/formHandler.js
@@ -4,34 +4,36 @@ const subjectivity = document.getElementById('subjectivity');
 const subConfidence = document.getElementById('sub-confidence');
 function handleSubmit(event) {
     event.preventDefault()
+    // check what text was put into the form field
     let formText = document.getElementById('name').value
     console.log("::: Form Submitted :::")
     fetchData(formText);
 }
 
-const fetchData = async (url = '') =>{
+const fetchData = async (url) =>{
     if(Client.vUrl(url))
     {
-            const res = await fetch("http://localhost:8081/scan", {
-                method: 'POST',
-                credentials: 'same-origin',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({url})
-            });
-            try {
-                const nData = await res.json();
-                return nData;
-            } catch (error) {
-                console.log(error);
-            }
+        console.log(JSON.stringify({'url':url}));
+        await fetch("http://localhost:8081/scan",{
+            method:"POST",
+            mode:'cors',
+            header:{
+                'Content-Type': 'application/json',
+                'accept':'application/json'
+            },
+            body: JSON.stringify({'url':url})
+        })
+        .then(res => res.json())
+        .then(function(res){
+            console.log(res);
+            update(res);
+        })
     }else{
         alert("erorr");
     }
 }
 const update = (data) =>{
-    //polarity.innerHTML =  data.polarity
+    polarity.innerHTML =  data.polarity
     polConfidence.innerHTML = data.polarity_confidence
     subjectivity.innerHTML =  data.subjectivity
     subConfidence.innerHTML =  data.subjectivity_confidence
--- evaluate-news-nlp/src/client/test/testNameCheaker.test.js
+++ evaluate-news-nlp/src/client/test/testNameCheaker.test.js
@@ -1,6 +1,7 @@
-import { checkForName } from "../js/nameChecker"
+import { urlChecker } from "../js/urlChecker"
+
 describe('Test "validateRequest()" should be a function' , () => {
     test('It should be a function', async () => {
-        expect(typeof checkForName).toBe("function");
+        expect(typeof vUrl).toBe("undefined");
     });
 });
